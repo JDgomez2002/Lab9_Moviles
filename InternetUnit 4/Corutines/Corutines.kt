c3425d323acd574899257e41f15382d9982155fd
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2022-10-11 00:06:07 -0600

    Corutines

---
+++ InternetUnit 4/Corutines/Corutines.kt
@@ -0,0 +1,16 @@
+//Jose Daniel Gomez Cabrera 21429
+
+import kotlinx.coroutines.*
+
+fun main() {
+   val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+   repeat(3) {
+       GlobalScope.launch {
+           println("${Thread.currentThread()} has started")
+           for (i in states) {
+               println("${Thread.currentThread()} - $i")
+               delay(5000)
+           }
+       }
+   }
+}
